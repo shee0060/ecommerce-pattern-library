3ff08f6a90ed652a8385e16adc9401eedfa8049e
shee0060 <Tori@Victorias-MacBook-Pro.local>
2018-03-11 21:45:42 -0400

    Add header dark to patterns

--- common/patternbot.js
+++ common/patternbot.js
@@ -348,9 +348,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/Tori/Desktop/Semester 4/Web Dev IV/ecommerce-pattern-library
- * @version 1520816431643
+ * @version 1520819092801
  */
-const patternManifest_1520816431643 = {
+const patternManifest_1520819092801 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -908,5 +908,5 @@ const patternManifest_1520816431643 = {
   }
 };
 
-patternBotIncludes(patternManifest_1520816431643);
+patternBotIncludes(patternManifest_1520819092801);
 }());
