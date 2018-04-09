5a58d9e260c6a28c690ad517d04e459679c4385a
krystyna-Macleod <macl0312@algonquinlive.com>
2018-04-09 10:05:49 -0400

    Add change pattern

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 11/ecommerce-pattern-library-krystyna
- * @version 1523281123965
+ * @version 1523281950305
  */
-const patternManifest_1523281123965 = {
+const patternManifest_1523281950304 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1097,5 +1097,5 @@ const patternManifest_1523281123965 = {
   }
 };
 
-patternBotIncludes(patternManifest_1523281123965);
+patternBotIncludes(patternManifest_1523281950304);
 }());
